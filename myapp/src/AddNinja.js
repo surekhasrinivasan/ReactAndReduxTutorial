4cfe4867a3d59ba56643e9609fea0dcd8f7c36a1
surekhasrinivasan <surekhasrao@gmail.com>
2018-09-24 13:00:56 -0400

    Lesson-18 Functions as Props - pass functions to nested components as props, then call those functions from the nested components, to interact with the state on the parent component

--- myapp/src/AddNinja.js
+++ myapp/src/AddNinja.js
@@ -13,7 +13,8 @@ class AddNinja extends Component {
     }
     handleSubmit =(e) => {
         e.preventDefault();
-        console.log(this.state);
+        //console.log(this.state);
+        this.props.addNinja(this.state);
     }
     render(){
         return (
--- myapp/src/App.js
+++ myapp/src/App.js
@@ -10,13 +10,22 @@ class App extends Component {
       { name: 'Crystal', age: 25, belt: 'pink', id: 3 }
     ]
   }
+  addNinja = (ninja) => {
+    //console.log(ninja);
+    ninja.id = Math.random();
+    //spread operator
+    let ninjas = [...this.state.ninjas, ninja]
+    this.setState({
+      ninjas: ninjas
+    })
+  }
   render() {
     return (
       <div className="App">
         <h1>My first React app!</h1>
         <p>Welcome :)</p>
         <Ninjas ninjas = { this.state.ninjas }/>
-        <AddNinja />
+        <AddNinja addNinja = { this.addNinja }/>
       </div>
     );
   }
