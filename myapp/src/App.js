f2ed121b0e2ad27444216171a43300e8702683a1
surekhasrinivasan <surekhasrao@gmail.com>
2018-09-25 12:32:33 -0400

    Lesson-22 Lifecycle Methods - lifecycle hooks/methods to fire code at certain points during our component lifecycle

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -29,6 +29,13 @@ class App extends Component {
       ninjas: ninjas
     })
   }
+  componentDidMount(){
+    console.log('component mounted');
+  }
+  componentDidUpdate(prevProps, prevState){
+    console.log('Component updated');
+    console.log(prevProps, prevState);
+  }
   render() {
     return (
       <div className="App">
